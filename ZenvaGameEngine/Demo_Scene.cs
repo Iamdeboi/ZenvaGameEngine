1cf02cc38f3d6c4a8ce87cc2f7721fa6b5fb002c
Dylan Del Sontro <dylandelsontro@gmail.com>
2026-02-08 13:18:53 -0500

    Labels added; score label added to the demo scene, updates properly

--- ZenvaGameEngine/Demo_Scene.cs
+++ ZenvaGameEngine/Demo_Scene.cs
@@ -14,6 +14,8 @@ namespace ZenvaGameEngine
         public override bool Init { get; set; }
 
         Player player;
+        int score = 0;
+        Label scoreLabel;
 
         public Demo_Scene(string levelName) : base(levelName)
         {
@@ -24,9 +26,10 @@ namespace ZenvaGameEngine
         {
             player = new Player(new Vector2(400, 400), new Vector2(50, 50), "player");
             player.CollisionDebug(true);
+            player.onCollisionEventHandlers.Add(Body_OnCollision);
             Shape2D box = new Shape2D(Shape2D.SHAPES.RECTANGLE, new Vector2(500, 500), new Vector2(50, 50), "box", Color.Transparent, Color.Red);
             Wall wall = new Wall(new Vector2(300, 400), new Vector2(50, 50), "wall");
-            player.onCollisionEventHandlers.Add(Body_OnCollision);
+            scoreLabel = new Label("Score: 0", 32, new Vector2(400,100), Color.White, "scoreLabel", true);
         }
 
 
@@ -38,7 +41,7 @@ namespace ZenvaGameEngine
 
         public override void OnUpdate()
         {
-            
+            scoreLabel.Text = ($"score: {score}");
         }
     }
 }
---
+++ ZenvaGameEngine/Source/Label.cs
@@ -0,0 +1,60 @@
+﻿using SFML.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZenvaGameEngine.Source
+{
+    internal class Label : GameObject
+    {
+        public override Vector2 Position { get ; set; }
+        public override Vector2 Origin { get; set; }
+        public override Vector2 Scale { get; set; }
+        public override string Tag { get; set; }
+        public override List<GameObject> Children { get; set; }
+
+        const string DEFAULTFONTPATH = "Assets/Default Font.ttf";
+        public string Text = "";
+        public string fontPath = ""; //for different fonts besides the default
+        public Font font = null;
+        public uint fontSize = 1;
+        public bool centered;
+        public Color color = Color.White;
+
+        public Label(string text, uint fontSize, Vector2 Position, Color color, string tag, bool centered)
+        {
+            this.Text = text;
+            this.fontSize = fontSize;
+            this.Position = Position;
+            this.color = color;
+            this.Tag = tag;
+            this.centered = centered;
+            font = new Font(DEFAULTFONTPATH);
+        }
+
+        public override void OnFree()
+        {
+            
+        }
+
+        public override void OnLoad()
+        {
+            
+        }
+
+        public override void OnUpdate()
+        {
+            Text text = new Text(Text, font, fontSize);
+            if (centered)
+            {
+                FloatRect textRect = text.GetLocalBounds(); //gets size/scale of label to assist in changing the origin of the text on the screen
+                text.Origin = new Vector2(textRect.Left + textRect.Width * 0.5f, textRect.Top + textRect.Height * 0.5f);
+            }
+            text.Position = Position;
+            text.Color = color;
+            Engine.app.Draw(text);
+        }
+    }
+}
